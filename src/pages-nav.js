c884fc86207d395e5f9b2a145f6082a9a2166c2c
nbeuve <nicolas.beuve@insa-rennes.fr>
2021-10-21 13:47:28 +0200

    add activate state for carousel buttons

--- src/pages-nav.js
+++ src/pages-nav.js
@@ -1,17 +1,42 @@
 function showContentInfos() {
     hide('slides');
+    deactivate_button('slides-button');
     hide('live');
+    deactivate_button('live-button');
     show('content-infos');
+    activate_button('infos-button');
 }
 function showSlides() {
     hide('live');
+    deactivate_button('live-button');
     hide('content-infos');
+    deactivate_button('infos-button');
     show('slides');
+    activate_button('slides-button');
 }
 function showLive() {
     hide('slides');
+    deactivate_button('slides-button');
     hide('content-infos');
+    deactivate_button('infos-button');
     show('live');
+    activate_button('live-button');
+}
+
+//"infos-button" "slides-button" "live-button"
+
+function deactivate_button(id) {
+    var el = document.getElementById(id);
+    if (el) {
+        el.classList.remove("active");
+    }
+}
+
+function activate_button(id) {
+    var el = document.getElementById(id);
+    if (el) {
+        el.classList.add("active");
+    }
 }
 
 function hide(id) {
